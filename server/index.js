79880340b153389476fb941e15ed2f4566602d19
seolleung2 <seolleung22@gmail.com>
2021-01-03 01:15:59 +0900

    start backend side with express, expressRouter(), and Socket.io

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const socketio = require("socket.io");
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
+
+const router = require("./router");
 
 const server = http.createServer(app);
 
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected!!🐼");
+  socket.on("disconnect", () => {
+    console.log("user disconnected!!😢");
+  });
+});
+
+app.use(router);
+
 server.listen(PORT, () => {
-  console.log(`Server Starts on ${PORT}`);
+  console.log(`Server HAS Started on port ${PORT}`);
 });
