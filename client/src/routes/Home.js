9408ce27aa98a1fce3635d425d88e4124fe44e1e
munawiki <munawiki@gamil.com>
2021-01-03 00:11:45 +0900

    componentWillUnmount 이슈 해결

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -24,10 +24,15 @@ class Home extends Component {
   componentDidMount() {
     // this.getData(); // 1. 기존 방법
     // window.setTimeout(window.location.reload(), 60000) // 2. window 창 자체를 새로고침 (매우 무식스)
-    setInterval(() => {
+    this._intervalID = setInterval(() => {
       this.getData(); // 3. 위에 this.getData = this.getData.bind(this) 를 안적어서 이래 표기하니 되었다.
     }, 1000);
   }
+
+  componentWillUnmount() {
+    clearInterval(this._intervalID);
+  }
+
   handleChange = (event) => {
     this.setState({
       comment: event.target.value,
